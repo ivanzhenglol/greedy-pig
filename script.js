558411f5e784126b30f05e7aac1ed36fe46c233c
ivanzhenglol <94770707+ivanzhenglol@users.noreply.github.com>
2021-12-12 16:11:31 +1300

    save

--- script.js
+++ script.js
@@ -8,22 +8,48 @@ const startGame = () => {
     scoreboard = [0,0];
 }
 
-const stringNew = document.querySelector('#score--0')
-const stringNew1 = document.querySelector('#score--1')
-const scoreNew1 = document.querySelector('.current-score')
-const scoreNew = document.querySelector('.current-score')
-
+const stringNew = document.querySelector('#score--0');
+const stringNew1 = document.querySelector('#score--1');
+const scoreNew1 = document.querySelector('.current-score');
+const scoreNew = document.querySelector('.current-score');
+const dice = document.querySelector('.dice');
+const player1 = document.querySelector('.player--0 ');
+const player2 = document.querySelector('.player--1 ');
+const butonRollDice = document.querySelector('.wbutton--roll');
+const buttonHold = document.querySelector('.button--hold');
 
 const startGame = () => {
     scoreboard = [0,0];
     currentScore = 0;
     activeplayer = 0;
     isGameRunning = true;
+    dice.classList.add('hidden');
+    player1.classList.remove('player--winner');
+    player2.classList.remove('player--winner');
+    player1.classList.add('player--Active');
+    player2.classList.remove('player--Active');
+  
 };
 
+const holdMyScore = () => {
+
+};
+const rollMyDice = () => {
+
+};
+const switchplayer = () => {
+
+}
 startGame();
 buttonNew.addEventListener('click',() => {
     startGame();
 });
 
+buttonRollDice.addEventListener('click',()=> {
+    rollMyDice();
+});
+
+buttonHold.addEventListener('click',()=> {
+    holdMyScore();
+});
 
